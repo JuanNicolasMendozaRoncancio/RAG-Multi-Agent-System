6cde80fce428609c21c9a119e135ab7592fc1fda
Juan Nicolas <juannicolasmendoza4@gmail.com>
2026-07-22 21:55:49 +0200

    First files

---
+++ shared/llm_client.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+def caht_complete(
+    messages: list[dict[str, str]],
+    *,
+    json_mode: bool = False
+) -> dict[str, object]:
+    """placeholder"""
+    raise NotImplementedError("llm_client not yet implemented")
